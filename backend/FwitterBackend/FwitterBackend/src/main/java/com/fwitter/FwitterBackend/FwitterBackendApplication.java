6b90f36541c5f90deb8a811ab6492c2ebdcd9502
Paritosh Pal <paritoshpal7796@gmail.com>
2025-02-06 09:41:28 +0530

    Project Setup - Backend

---
+++ backend/FwitterBackend/FwitterBackend/src/main/java/com/fwitter/FwitterBackend/FwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fwitter.FwitterBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwitterBackendApplication.class, args);
+	}
+
+}
---
+++ backend/FwitterBackend/FwitterBackend/src/test/java/com/fwitter/FwitterBackend/FwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwitter.FwitterBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
