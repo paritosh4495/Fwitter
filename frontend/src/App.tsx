0159060c7e020f346e0b7b70e11a580ea3f5dd8c
Paritosh Pal <paritoshpal7796@gmail.com>
2025-02-06 22:02:28 +0530

    React, Pages and Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import './assets/global.css'
+import { Home } from './pages/Home'
 
 export const App = () => {
   return (
-    <div>HELLO</div>
+    <div><Home/></div>
+   
   )
 }
---
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Modal } from '../../../../components/Modal/Modal'
+
+export const RegisterModal:React.FC = () => {
+  return (
+    <div className='register-modal'>
+        <Modal>
+            <h1>Regiser MODAL</h1>
+        </Modal>
+    </div>
+  )
+}
+
