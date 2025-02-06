c87206a7d39fbc6bd67eace12c4a8f2ab2bec60b
Paritosh Pal <paritoshpal7796@gmail.com>
2025-02-06 10:03:07 +0530

    User and Role Model

---
+++ backend/FwitterBackend/FwitterBackend/src/main/java/com/fwitter/FwitterBackend/models/ApplicationUser.java
@@ -0,0 +1,71 @@
+package com.fwitter.FwitterBackend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class ApplicationUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+
+    @Column(name = "dob")
+    private Date dateOfBirth;
+
+    @Column(unique = true)
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role_junction",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
+    )
+    private Set<Role> authorities;
+
+
+    public ApplicationUser(){
+        this.authorities = new HashSet<>();
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationUser{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", authorities=" + authorities +
+                '}';
+    }
+}
---
+++ backend/FwitterBackend/FwitterBackend/src/main/java/com/fwitter/FwitterBackend/models/Role.java
@@ -0,0 +1,31 @@
+package com.fwitter.FwitterBackend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Integer id;
+
+    private String authority;
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
